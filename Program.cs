fe48af7ce965109ec56b637320a4ea0edae72910
Bhummie <adebolaolubunmi32@gmail.com>
2022-03-09 01:36:33 +0200

    implemented task 10

--- Program.cs
+++ Program.cs
@@ -4,29 +4,57 @@ namespace TDETestTask10
 {
 	class Program
 	{
+		static int counter = 0;
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string input = "aba";
 
-			string input = "hello";
-			char[] inputChar = getReversedStr(input);
-
-			string str = getMisMatch(inputChar);
+			string str = Match_pair(input);
 			Console.WriteLine(str);
+			
 		}
 
-		private static char[] getReversedStr(string input)
+		public static string Match_pair(string input)
+		{
+			int i = 0;
+			int j = input.Length;
+
+			if (counter == 0 && input.Length > 4) // palindrome only makes sense when there's more than 2 characters
+			{// plus its not wise to make this much computations if palindrome is short 
+				string reversedInput = getReversedStr(input);
+
+				if (input.Equals(reversedInput))
+				{
+					return "";
+				}
+			}
+
+			if (input.Length > 0)
+			{
+				if (input[i] != input[input.Length - 1])
+				{
+					return new string(input[i] + "" + input[j - 1]);
+				}
+				else if (j == 1 || i == (j - 1)) 
+				{ 
+					return ""; 
+				}
+
+				i++;
+				counter++;
+				return Match_pair(input.Substring(i, j - 2)); 
+			}
+
+			counter = 0; // clear global variable
+			return "";
+		}
+
+		private static string getReversedStr(string input)
 		{
 			char[] inputChar = input.ToCharArray();
 			Array.Reverse(inputChar);
-			return inputChar;
+			return new string(inputChar);
 		}
 
-		public static string getMisMatch(char[] input)
-		{
-			int i = 0;
-
-			return "{i}";
-		}
 	}
 }
