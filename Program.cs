e0d4303621537f0e25dc08cfda5d076b6842345d
Bhummie <adebolaolubunmi32@gmail.com>
2022-03-08 23:54:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TDETestTask10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			string input = "hello";
+			char[] inputChar = getReversedStr(input);
+
+			string str = getMisMatch(inputChar);
+			Console.WriteLine(str);
+		}
+
+		private static char[] getReversedStr(string input)
+		{
+			char[] inputChar = input.ToCharArray();
+			Array.Reverse(inputChar);
+			return inputChar;
+		}
+
+		public static string getMisMatch(char[] input)
+		{
+			int i = 0;
+
+			return "{i}";
+		}
+	}
+}
